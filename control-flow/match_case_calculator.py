83f0ae9d9aea535426d7c78e0c0d2a7890a93502
Ecode2 <ecode5814@gmail.com>
2024-07-03 16:56:38 +0100

    matching

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,16 +2,16 @@ num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    print(f"The result is {num1 + num2}")
-
-elif operation == "-":
-    print(f"The result is {num1 - num2}")
-
-elif operation == "*":
-    print(f"The result is {num1 * num2}")
-
-elif operation == "/":
-    if num2 <= 0:
-        print("Cannot divide by zero")
-    print(f"The result is {num1 - num2}")
+match operation:
+    case "+":
+        print(f"The result is {num1 + num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
+    case "*":
+        print(f"The result is {num1 * num2}")
+    case "/":
+        if num2 <= 0:
+            print("Cannot divide by zero")
+        print(f"The result is {num1 - num2}")
+    case _:
+        print("Invalid day entered.")
