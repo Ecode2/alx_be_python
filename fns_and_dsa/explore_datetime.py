7735d367a13991988aaa6d3ee274745f36642451
Abubakar Alaya <ecode5814@gmail.com>
2024-07-11 12:59:59 +0100

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import datetime, timedelta
 
 def calculate_future_date():
     future_date = int(input("Enter the number of days to add to the current date: "))
-    return datetime.timedelta(days=future_date)
+    return timedelta(days=future_date)
 
 def display_current_datetime():
-    current_date = datetime.datetime.utcnow()
+    current_date = datetime.utcnow()
     print(current_date)
 
     days = calculate_future_date()
